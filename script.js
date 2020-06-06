6a5a9ebc6b86fe8b657559e3ce62f037c2927a88
felipebmuniz <felipemuniz@alu.ufc.br>
2020-06-06 20:04:42 -0300

    desaparece a fruta e aumenta a cobrinha

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ snake[0] = {
 
 let direction = 'right';
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBg () {
     context.fillStyle = 'lightgray';
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -19,6 +24,11 @@ function criarCobrinha () {
         context.fillStyle = 'blue';
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood () {
+    context.fillStyle = 'purple';
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -38,6 +48,7 @@ function iniciarGame () {
 
     criarBg();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -47,7 +58,12 @@ function iniciarGame () {
     if (direction == 'up') snakeY -= box;
     if (direction == 'down') snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
